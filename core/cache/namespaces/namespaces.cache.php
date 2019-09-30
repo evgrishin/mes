734bda809aa2427fa278a429cdf1aafd236dbae5
Evgeny <e.v.grishin@yandex.ru>
2019-09-30 10:11:40 +0300

    gmi

---
+++ core/cache/namespaces/namespaces.cache.php
@@ -0,0 +1,20 @@
+<?php  return array (
+  'core' => 
+  array (
+    'name' => 'core',
+    'path' => 'C:/domains/newprojects/mega-son/www/manager/',
+    'assets_path' => 'C:/domains/newprojects/mega-son/www/manager/assets/',
+  ),
+  'minishop2' => 
+  array (
+    'name' => 'minishop2',
+    'path' => 'C:/domains/newprojects/mega-son/www/core/components/minishop2/',
+    'assets_path' => '',
+  ),
+  'pdotools' => 
+  array (
+    'name' => 'pdotools',
+    'path' => 'C:/domains/newprojects/mega-son/www/core/components/pdotools/',
+    'assets_path' => '',
+  ),
+);
