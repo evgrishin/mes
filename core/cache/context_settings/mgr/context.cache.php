c5dc365d6ddcdc2b692b10e451d16ff810310a17
Evgeny <e.v.grishin@yandex.ru>
2019-09-30 10:01:52 +0300

    ms2 installed

--- core/cache/context_settings/mgr/context.cache.php
+++ core/cache/context_settings/mgr/context.cache.php
@@ -12,13 +12,34 @@
   ),
   'eventMap' => 
   array (
+    'msOnChangeOrderStatus' => 
+    array (
+      2 => '2',
+    ),
+    'OnHandleRequest' => 
+    array (
+      2 => '2',
+    ),
+    'OnLoadWebDocument' => 
+    array (
+      2 => '2',
+    ),
     'OnMODXInit' => 
     array (
       1 => '1',
+      2 => '2',
     ),
     'OnSiteRefresh' => 
     array (
       1 => '1',
+    ),
+    'OnUserSave' => 
+    array (
+      2 => '2',
+    ),
+    'OnWebPageInit' => 
+    array (
+      2 => '2',
     ),
     'OnWebPagePrerender' => 
     array (
@@ -74,6 +95,130 @@ switch ($modx->event->name) {
       'moduleguid' => '',
       'static' => '0',
       'static_file' => 'core/components/pdotools/elements/plugins/plugin.pdotools.php',
+    ),
+    2 => 
+    array (
+      'id' => '2',
+      'source' => '1',
+      'property_preprocess' => '0',
+      'name' => 'miniShop2',
+      'description' => '',
+      'editor_type' => '0',
+      'category' => '2',
+      'cache_type' => '0',
+      'plugincode' => '/** @var modX $modx */
+switch ($modx->event->name) {
+
+    case \'OnMODXInit\':
+        // Load extensions
+        /** @var miniShop2 $miniShop2 */
+        if ($miniShop2 = $modx->getService(\'miniShop2\')) {
+            $miniShop2->loadMap();
+        }
+        break;
+
+    case \'OnHandleRequest\':
+        // Handle ajax requests
+        $isAjax = !empty($_SERVER[\'HTTP_X_REQUESTED_WITH\']) && $_SERVER[\'HTTP_X_REQUESTED_WITH\'] == \'XMLHttpRequest\';
+        if (empty($_REQUEST[\'ms2_action\']) || !$isAjax) {
+            return;
+        }
+        /** @var miniShop2 $miniShop2 */
+        if ($miniShop2 = $modx->getService(\'miniShop2\')) {
+            $response = $miniShop2->handleRequest($_REQUEST[\'ms2_action\'], @$_POST);
+            @session_write_close();
+            exit($response);
+        }
+        break;
+
+    case \'OnLoadWebDocument\':
+        // Handle non-ajax requests
+        if (!empty($_REQUEST[\'ms2_action\'])) {
+            if ($miniShop2 = $modx->getService(\'miniShop2\')) {
+                $miniShop2->handleRequest($_REQUEST[\'ms2_action\'], @$_POST);
+            }
+        }
+        // Set product fields as [[*resource]] tags
+        if ($modx->resource->get(\'class_key\') == \'msProduct\') {
+            if ($dataMeta = $modx->getFieldMeta(\'msProductData\')) {
+                unset($dataMeta[\'id\']);
+                $modx->resource->_fieldMeta = array_merge(
+                    $modx->resource->_fieldMeta,
+                    $dataMeta
+                );
+            }
+        }
+        break;
+
+    case \'OnWebPageInit\':
+        // Set referrer cookie
+        /** @var msCustomerProfile $profile */
+        $referrerVar = $modx->getOption(\'ms2_referrer_code_var\', null, \'msfrom\', true);
+        $cookieVar = $modx->getOption(\'ms2_referrer_cookie_var\', null, \'msreferrer\', true);
+        $cookieTime = $modx->getOption(\'ms2_referrer_time\', null, 86400 * 365, true);
+
+        if (!$modx->user->isAuthenticated() && !empty($_REQUEST[$referrerVar])) {
+            $code = trim($_REQUEST[$referrerVar]);
+            if ($profile = $modx->getObject(\'msCustomerProfile\', array(\'referrer_code\' => $code))) {
+                $referrer = $profile->get(\'id\');
+                setcookie($cookieVar, $referrer, time() + $cookieTime);
+            }
+        }
+        break;
+
+    case \'OnUserSave\':
+        // Save referrer id
+        if ($mode == modSystemEvent::MODE_NEW) {
+            /** @var modUser $user */
+            $cookieVar = $modx->getOption(\'ms2_referrer_cookie_var\', null, \'msreferrer\', true);
+            $cookieTime = $modx->getOption(\'ms2_referrer_time\', null, 86400 * 365, true);
+            if ($modx->context->key != \'mgr\' && !empty($_COOKIE[$cookieVar])) {
+                if ($profile = $modx->getObject(\'msCustomerProfile\', array(\'id\' => $user->get(\'id\')))) {
+                    if (!$profile->get(\'referrer_id\') && $_COOKIE[$cookieVar] != $user->get(\'id\')) {
+                        $profile->set(\'referrer_id\', (int)$_COOKIE[$cookieVar]);
+                        $profile->save();
+                    }
+                }
+                setcookie($cookieVar, \'\', time() - $cookieTime);
+            }
+        }
+        break;
+
+    case \'msOnChangeOrderStatus\':
+        // Update customer stat
+        if (empty($status) || $status != 2) {
+            return;
+        }
+
+        /** @var modUser $user */
+        if ($user = $order->getOne(\'User\')) {
+            $q = $modx->newQuery(\'msOrder\', array(\'type\' => 0));
+            $q->innerJoin(\'modUser\', \'modUser\', array(\'modUser.id = msOrder.user_id\'));
+            $q->innerJoin(\'msOrderLog\', \'msOrderLog\', array(
+                \'msOrderLog.order_id = msOrder.id\',
+                \'msOrderLog.action\' => \'status\',
+                \'msOrderLog.entry\' => $status,
+            ));
+            $q->where(array(\'msOrder.user_id\' => $user->get(\'id\')));
+            $q->groupby(\'msOrder.user_id\');
+            $q->select(\'SUM(msOrder.cost)\');
+            if ($q->prepare() && $q->stmt->execute()) {
+                $spent = $q->stmt->fetchColumn();
+                /** @var msCustomerProfile $profile */
+                if ($profile = $modx->getObject(\'msCustomerProfile\', array(\'id\' => $user->get(\'id\')))) {
+                    $profile->set(\'spent\', $spent);
+                    $profile->save();
+                }
+            }
+        }
+        break;
+}',
+      'locked' => '0',
+      'properties' => NULL,
+      'disabled' => '0',
+      'moduleguid' => '',
+      'static' => '0',
+      'static_file' => 'core/components/minishop2/elements/plugins/plugin.minishop2.php',
     ),
   ),
   'policies' => 
@@ -263,6 +408,27 @@ switch ($modx->event->name) {
             'view_unpublished' => true,
             'view_user' => true,
             'workspaces' => true,
+          ),
+        ),
+        1 => 
+        array (
+          'principal' => 1,
+          'authority' => 9999,
+          'policy' => 
+          array (
+            'mscategory_save' => true,
+            'msproduct_save' => true,
+            'msproduct_publish' => true,
+            'msproduct_delete' => true,
+            'msorder_save' => true,
+            'msorder_view' => true,
+            'msorder_list' => true,
+            'mssetting_save' => true,
+            'mssetting_view' => true,
+            'mssetting_list' => true,
+            'msproductfile_save' => true,
+            'msproductfile_generate' => true,
+            'msproductfile_list' => true,
           ),
         ),
       ),
